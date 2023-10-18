44b45a70fdca14f7d7a3b3382c8deed6b4901e47
ihossen016 <ihossen016@gmail.com>
2023-10-18 21:34:26 +0600

    title renamed

--- src/stories/ReactNextPagination.stories.js
+++ src/stories/ReactNextPagination.stories.js
@@ -1,9 +1,8 @@
 import { ReactNextPagination } from "../components/ReactNextPagination";
 
-//👇 This default export determines where your story goes in the story list
 export default {
     component: ReactNextPagination,
-    title: "Pagination",
+    title: "React Next Pagination",
     props: {
         baseDir: "products",
         subDir: "page",
@@ -14,7 +13,6 @@ export default {
 
 export const Primary = {
     args: {
-        //👇 The args you need here will depend on your component
         baseDir: "products",
         subDir: "page",
         totalPage: 20,
